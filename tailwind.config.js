0fe97dcd66ee29f6ff1c78c2b8070d21c386125b
Merijn <prim@pr1mula.nl>
2023-05-03 18:14:06 +0100

    space en phone nummer

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,7 @@ module.exports = {
         },
       },
       backgroundImage: {
-        hero: "url('/src/img/hero/bg.JPG')",
+        hero: "url('/src/img/hero/bg.jpg')",
         about: "url('/src/img/about/image.png')",
         interview: "url('/src/img/interview/bg.png')",
         footer: "url('/src/img/footer/shop.jpg')",
